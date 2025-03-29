f4322cbf89bd9757f9479202c5f1864b8ebd839c
Jean-Marc Boulos <jeanmarc.boulos@gmail.com>
2025-03-29 11:58:41 +0200

    swr issue -29/03/2025

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,8 +7,12 @@ import CartIcon from "./CartIcon";
 import FavoriteButton from "./FavoriteButton";
 import SignIn from "./SignIn";
 import MobileMenu from "./MobileMenu";
+import { currentUser } from "@clerk/nextjs/server";
+import { ClerkLoaded } from "@clerk/nextjs";
+import { SignedIn, UserButton } from "@clerk/clerk-react";
 
-const Header = () => {
+const Header = async () => {
+  const user = await currentUser();
   return (
     <header className="bg-white py-5 border-b border-b-black/20">
       <Container className="flex items-center justify-between text-lightColor">
@@ -22,7 +26,12 @@ const Header = () => {
           <SearchBar />
           <CartIcon />
           <FavoriteButton />
-          <SignIn />
+          <ClerkLoaded>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+            {!user && <SignIn />}
+          </ClerkLoaded>
         </div>
       </Container>
     </header>
