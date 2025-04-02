b6dc970c9dfea79c018d4187fe317f3449daf8c3
Jean-Marc Boulos <jeanmarc.boulos@gmail.com>
2025-04-02 20:46:50 +0300

    Created new flex and items - initialized new global css colors - added new logic and pages -02/04/2025

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -1,10 +1,13 @@
 import { Product } from "@/sanity.types";
 import { urlFor } from "@/sanity/lib/image";
-import { Flame } from "lucide-react";
+import { Flame, StarIcon } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import AddToWishlistButton from "./AddToWishlistButton";
+import { Title } from "./ui/text";
+import PriceView from "./PriceView";
+import AddToCartButton from "./AddToCartButton";
 
 const ProductCard = ({ product }: { product: Product }) => {
   return (
@@ -46,8 +49,44 @@ const ProductCard = ({ product }: { product: Product }) => {
       </div>
       <div className="p-3 flex flex-col gap-2">
         {product?.categories && (
-          <p>{product?.categories?.map((cat) => cat).join(",")}</p>
+          <p className="uppercase line-clamp-1 text-xs text-shop_light_text">
+            {product?.categories?.map((cat) => cat).join(",")}
+          </p>
         )}
+        <Title className="text-sm line-clamp-1">{product?.name}</Title>
+        <div className="flex items-center gap-2">
+          <div className="flex items-center gap-0.5">
+            {[...Array(5)].map((_, index) => (
+              <StarIcon
+                size={12}
+                key={index}
+                className={
+                  index < 4
+                    ? "text-shop_lighter_green"
+                    : "text-shop_lighter_text"
+                }
+                fill={index < 4 ? "#93D991" : "#ababab"}
+              />
+            ))}
+          </div>
+          <p className="text-shop_light_text text-xs tracking-wide">
+            5 Reviews
+          </p>
+        </div>
+        <div className="flex items-center gap-2.5">
+          <p className="font-medium">In Stock</p>
+          <p
+            className={`font-semibold ${product?.stock === 0 ? "text-red-600" : "text-shop_light_green/80"}`}
+          >
+            {(product?.stock as number) > 0 ? product?.stock : "Unavailable"}
+          </p>
+        </div>
+        <PriceView
+          price={product?.price}
+          discount={product?.discount}
+          className="text-sm"
+        />
+        <AddToCartButton product={product} className="w-36 rounded-full"/>
       </div>
     </div>
   );
