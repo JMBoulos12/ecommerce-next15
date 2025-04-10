0070062d35ccf861ddf8a8bef851bbd9e5db530a
JMBoulos <jeanmarc8boulos@gmail.com>
2025-04-10 23:00:53 +0300

    Dynamic Shop Component Integration with Real-Time Filtering + Programmatic Shop Page Rendering and Filter Logic Implementation -10/04/2025

--- components/HomeBanner.tsx
+++ components/HomeBanner.tsx
@@ -9,8 +9,8 @@ const HomeBanner = () => {
     <div className="py-16 md:py-0 bg-shop_light_pink rounded-lg px-10 lg:px-24 flex items-center justify-between">
       <div className="space-y-5">
         <Title>
-          Grab Upto 50% off on <br />
-          Selected headphone
+          Grab Upto 75% off on <br />
+          Smart Gadgets
         </Title>
         <Link
           href={"/shop"}
